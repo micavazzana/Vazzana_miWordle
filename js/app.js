df77d11c1d82cfb1d535754d9af64226cc99bcf7
micavazzana <micaela.vazzana@gmail.com>
2023-07-29 00:37:24 -0300

    agrego funcionalidad del enter

--- js/app.js
+++ js/app.js
@@ -7,34 +7,46 @@ document.getElementById("btn-info").addEventListener("click", displayInfo);
 document.getElementById("cerrar-ajustes").addEventListener("click", displayJuego);
 document.getElementById("cerrar-info").addEventListener("click", displayJuego);
 
-/****** Display de Ajustes, Info y Juego *********/
+/********* Display de Ajustes, Info y Juego *********/
 //Comienza mostrando el juego y ocultando los ajustes e info
-sectionAjustes.style.display = "none";
-sectionInfo.style.display = "none";
+displayJuego();
 
+/**
+ * Muestra la sección Ajustes.
+ * Oculta la sección Juego e Info.
+ */
 function displayAjustes() {
    sectionAjustes.style.display = "";
    sectionJuego.style.display = "none";
    sectionInfo.style.display = "none";
 }
+/**
+ * Muestra la sección Info.
+ * Oculta la sección Juego y Ajustes.
+ */
 function displayInfo() {
    sectionInfo.style.display = "";
    sectionJuego.style.display = "none";
    sectionAjustes.style.display = "none";
 }
-
+/**
+ * Muestra la sección Juego.
+ * Oculta la sección Ajustes e Info.
+ */
 function displayJuego() {
    sectionJuego.style.display = "";
    sectionAjustes.style.display = "none";
    sectionInfo.style.display = "none";
 }
 
-/************** DOM del juego *****************/
+/************** Control de Casilleros *****************/
 
-//Variable que manejara los indices de los casilleros
+//Variables que manejaran los indices de los casilleros y los numeros de fila
 let index = 0;
+let numeroFila = 1;
 
-//Creo una lista de todos los nodos boton. Según la cantidad de letras elegidas se crearan los casilleros
+//Creo una lista de todos los nodos boton. Agrego un escuchador para cada boton.
+//Según el boton seleccionado sera la cantidad de letras elegidas. A partir de estas se crearan los casilleros
 const listBtn = document.querySelectorAll(".cant-letras button");
 listBtn.forEach((btn) => btn.addEventListener("click", crearCasilleros));
 
@@ -42,26 +54,26 @@ listBtn.forEach((btn) => btn.addEventListener("click", crearCasilleros));
 const btn4Letras = document.getElementById("btn4-letras");
 crearCasilleros.call(btn4Letras);
 
-//Suscribo la función "handlerKeydown" al evento keydown.
-window.addEventListener("keydown", handlerKeydown);
+//Suscribo la función "procesarTeclaPresionada" al evento keydown.
+window.addEventListener("keydown", procesarTeclaPresionada);
 
 /**
  * Se ocupa de crear los casilleros segun la cantidad de letras elegida.
  * Internamente ademas de crear los casilleros se ocupa de cambiar el estilo del botón seleccionado
  */
 function crearCasilleros() {
-
    //Obtengo la cantidad de casilleros a crear desde el atributo cantidad que estableci en el html
    let cantidad = parseInt(this.getAttribute("cantidad"));
 
    //Borro lo que hay en el inner del contenedor para crear los casilleros que corresponden cada vez
    const contenedor = document.querySelector(".contenedor-filas");
    contenedor.innerHTML = "";
+   //Reseteo el indice que manejara los casilleros esten en 0 cada vez
    index = 0;
+   numeroFila = 1;
 
    //Creo las filas segun la cantidad de intentos del juego
    for (let i = 0; i < 6; i++) {
-
       //Creo un div para cada fila
       const divFila = document.createElement("div");
       divFila.className = "fila centrar";
@@ -71,15 +83,11 @@ function crearCasilleros() {
       for (let j = 0; j < cantidad; j++) {
          const div = document.createElement("div");
          div.className = "casillero centrar";
-
-         //Apendeo cada casillero a la fila
          divFila.appendChild(div);
       }
-
-      //Apendeo cada fila al contenedor
-      contenedor.appendChild(divFila);
+      contenedor.appendChild(divFila); //Apendeo cada fila al contenedor
    }
-   
+
    cambiarEstiloBotonSeleccionado(this);
 }
 
@@ -87,66 +95,68 @@ function crearCasilleros() {
  * Cambia el estilo del boton seleccionado
  * @param {node} boton - Elemento nodo del DOM. Representa un botón.
  */
-function cambiarEstiloBotonSeleccionado(boton){   
+function cambiarEstiloBotonSeleccionado(boton) {
    listBtn.forEach((btn) => {
+      //Si el boton es el seleccionado le añado la clase "seleccionado"
       if (btn == boton)
-         //Si el boton es el seleccionado le añado la clase "seleccionado"
          btn.classList.add("seleccionado");
       //Sino remuevo de todos los botones la clase para que no queden coloreados en la siguiente eleccion.
-      else btn.classList.remove("seleccionado");
+      else 
+         btn.classList.remove("seleccionado");
    });
 }
 
 /**
  * Función que se suscribe al evento "keydown".
  * Se ocupará de obtener lo que el usuario teclea y colocarlo dentro de los casilleros.
- * Si el usuario escribe se ocupará de incrementar los indices validando que no quede fuera del rango, 
+ * Si el usuario escribe se ocupará de incrementar los indices validando que no quede fuera del rango,
  * por el contrario si borra tambien validará lo mismo. Por otra parte también valida que lo ingresado sean letras válidas.
  * @param {Object} event - Objeto event que tiene la información del evento que ocurrió. En este caso la tecla presionada.
  */
-function handlerKeydown(event){
-
+function procesarTeclaPresionada(event) {
    //Obtengo la fila correspondiente a ser jugada
-   const fila = document.getElementById("fila1");//Ver de como obtener la fila segun los intentos
+   const fila = document.getElementById(`fila${numeroFila}`);
    const casilleros = fila.children; //me guardo los hijos nodo de la fila, los casilleros
 
-      //Si el usuario decide borrar
-      if(event.key == "Backspace" || event.key == "Delete")
-      {
-         //Chequeo que el indice no sea negativo
-         if (index > 0)
-         {
-            index--;
-            const casillero = casilleros[index];
-            casillero.textContent = "";
-            console.log(index);
-         }
+   //Si el usuario decide borrar
+   if (event.key == "Backspace" || event.key == "Delete") {
+      //Chequeo que el indice no sea negativo
+      if (index > 0) {
+         index--;
+         const casillero = casilleros[index];
+         casillero.textContent = "";
+         console.log(index);
       }
-      //Si por el contrario escribe
-      else 
-      {
-         //Chequeo que el indice no sean mayor a la cantidad de casilleros y que lo que ingresa el usuario sea valido
-         if(index < casilleros.length && event.key >= 'a' && event.key <= 'z' || event.key == 'ñ') 
-         {  
-            const casillero = casilleros[index];
-            casillero.textContent = event.key;
-            index = index + 1;
-            console.log(index);
-         } 
+   }
+   //Si por el contrario escribe
+   else {
+      //Chequeo que el indice no sean mayor a la cantidad de casilleros y que lo que ingresa el usuario sea valido
+      if (index < casilleros.length && (event.key >= "a" && event.key <= "z") || event.key == "ñ") {
+         const casillero = casilleros[index];
+         casillero.textContent = event.key.toUpperCase();
+         index = index + 1;
+         console.log(index);
       }
+      //Chequeo si termino de escribir y quiere probar la palabra
+      if (casilleros.length == index && event.key == "Enter") {
+         chequearPalabra(casilleros);
+         numeroFila++; //Fijarme de controlar que si llega a 6 se acabaron los intentos
+         index = 0;
+         //podria meter una animacion
+      }
+   }
 }
 
-
-
-
-
-
-
-// function RefrescarFila(palabra) {
-//    for (let i = 0; i < casilleros.length; i++) {
-//       RefrescarCasillero(casilleros[i],palabra[i]);
-//    }
-// } 
-// function RefrescarCasillero(casillero,letra) {
-//    casillero.textContent = letra;
-// } 
+/**
+ * Controla que la palabra exista
+ * @param {HTMLCollection} casilleros
+ */
+function chequearPalabra(casilleros) {
+   let palabra = "";
+   for (const div of casilleros) {
+      palabra += div.textContent;
+   }
+   //Chequear que la palabra exista.
+   //Analizar que letras estan bien y cuales mal
+   //cambiar el estilo de cada letra
+}
