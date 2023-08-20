cca488e71c2da47a5a59aab64cf690d1377bcb2b
micavazzana <micaela.vazzana@gmail.com>
2023-08-20 20:15:37 -0300

    segunda prueba de incorporacion json

--- js/palabra.js
+++ js/palabra.js
@@ -7,19 +7,48 @@ class Palabra {
    }
 }
 
-let arrayPalabras = [];
+const arrayPalabras = [];
 
+// const cargarPalabras = async() => {
+//    const resp = await fetch("../data/palabras.json");
+//    const data = await resp.json();
+//    arrayPalabras.push(...data.palabras.map(palabra => new Palabra(palabra.toUpperCase())))
+// }
+// cargarPalabras();
+
+// function cargarPalabras(){
+//    return fetch("../data/nuevo.json").then(response => response.json());
+// }
+// cargarPalabras().then(data => {
+//    arrayPalabras.push(...data.map(elemento => new Palabra(elemento.palabra.toUpperCase())))
+// })
+
+const cargarPalabras = async() => {
+   const resp = await fetch("../data/nuevo.json");
+   const data = await resp.json();
+   arrayPalabras.push(...data.map(elemento => new Palabra(elemento.toUpperCase())));
+   console.log(resp);
+}
+cargarPalabras();
+
+export { arrayPalabras }; //Exporto el array de objetos palabra
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Entrega anterior:
 // const data = JSON.parse(
 //    '{"palabras":["abaa","ababol","abaca","abacal","abacera","abaceria","abacero","abacial","abaco","abacora","abacorar","abad","abada","abadejo","abadenga","abadengo","abadesa","abadi","abadia","abadiado","abadiato","aeterno","abajar","abaje\u00F1a","abaje\u00F1o","abajera","abajo","abakua","abalada"]}'
 // );
 // arrayPalabras.push(...data.palabras.map(palabra => new Palabra(palabra.toUpperCase())))
-
-const cargarPalabras = async() => {
-   const resp = await fetch('../data/palabras.json');
-   const data = await resp.json();
-   arrayPalabras.push(...data.palabras.map(palabra => new Palabra(palabra.toUpperCase())))
-} 
-
-cargarPalabras();
-
-export { arrayPalabras }; //Exporto el array de objetos palabra
